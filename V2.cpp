0ff0785eef1b32631e4ff2aa48f8a25d2f857a21
Elisée Leydier <92147165+EscalierE@users.noreply.github.com>
2022-01-29 13:45:49 +0100

    Update V2.cpp

--- V2.cpp
+++ V2.cpp
@@ -5,40 +5,46 @@
 
 
 using namespace std;
+using namespace chrono;
 
 //Quelque soit un entier naturel n (pair), n est la somme de deux nombres premiers (n1 et n2).
 //PB: quelque soit n, afficher (stocker) toutes les pairs (n1, n2).
 
 //fonction qui reprend le crible d'eratosthène qui permet de faire un vecteur de nmbr premier
 
-void crible2(vector <unsigned> &vecPremier){
-    //creer un vecteur des nombre de 2 a n
+void affichervect(vector<unsigned> tab){
+    for(unsigned i = 0; i < tab.size(); ++i){
+        cout << tab[i] << "    "    ;
+    }
+    cout << endl;
+}
+void affichierAddition(const unsigned nombre, vector <unsigned> &addition){
+    for(unsigned i = 0; i < addition.size(); ++i){
+        cout << nombre << " = "
+             << addition[i] << " + "
+             << nombre - addition[i] << endl ;
+    }
+}
+
+int main(){
+
+    high_resolution_clock::time_point start = high_resolution_clock::now();
+    const unsigned nombre = 16;
+    assert(nombre%2 ==0);//on vérifie que n soit paire
+    vector <unsigned> premier(nombre - 1); //vecteur de nombre premier
+    premier[0] = 1;
+    premier [1] = 1;
     unsigned index = 2;
-    //parcourir le vecteur
-    while(index < vecPremier.size()){
-        //cout << vecPremier[index] << "zzz\n";
-        if (vecPremier[index] != 1){//si le nombre n'est pas 1,
-            //cout << "aaaa\n";
+    while(index < premier.size()){
+        if (premier[index] != 1){//si le nombre n'est pas 1,
             unsigned i = index * 2;
-            while(i < vecPremier.size()){  // on enleve tous ses multiples dans vecteur
-                vecPremier[i] = 1;
+            while(i < premier.size()){  // on enleve tous ses multiples dans vecteur
+                premier[i] = 1;
                 i += index;
             }
         }
         index += 1;
     }
-
-}
-
-int main(){
-
-    chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
-    unsigned nombre = 1E9;
-    assert(nombre%2 ==0);//on vérifie que n soit paire
-    vector <unsigned> premier(nombre - 1); //vecteur de nombre premier
-    premier[0] = 1;
-    premier [1] = 1;
-    crible2(premier);
     //affichervect(premier);
 
     vector <unsigned> addition; //vecteur avec toute les additions
@@ -47,12 +53,11 @@ int main(){
         unsigned val = nombre - i;
 
         if (premier[val] == 0 && premier[i] == 0) {
-            //cout << i << " + " << val << " = " <<  nombre << endl;
-            addition.push_back({premier[val]});
+            addition.push_back({val});
         }
     }
-    chrono::high_resolution_clock::duration elapsed = chrono::high_resolution_clock::now()-start;
-    cout << chrono::duration_cast<chrono::milliseconds>(elapsed).count() << " ms" << endl;
-
+    high_resolution_clock::duration elapsed = high_resolution_clock::now()-start;
+    cout << duration_cast<milliseconds>(elapsed).count() << " ms" << endl;
+    affichierAddition(nombre, addition);
     return 0;
 }
